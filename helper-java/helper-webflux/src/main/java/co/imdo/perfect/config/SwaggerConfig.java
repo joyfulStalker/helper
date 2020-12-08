a8b83d5a3c0a40122bd47b57e2c9d2f35418f4cc
liusl <liusl@visionet.com.cn>
2020-12-08 17:18:05 +0800

    swagger

--- helper-java/helper-webflux/src/main/java/co/imdo/perfect/config/SwaggerConfig.java
+++ helper-java/helper-webflux/src/main/java/co/imdo/perfect/config/SwaggerConfig.java
@@ -12,6 +12,7 @@ import reactor.core.publisher.Mono;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.schema.AlternateTypeRule;
 import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.schema.WildcardType;
@@ -19,14 +20,15 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 @Configuration
-@EnableSwagger2WebFlux
+@EnableOpenApi
+@EnableSwagger2
 public class SwaggerConfig {
 
 
