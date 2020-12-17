8d55a1e831d5bf82a330a8baad21fa91c8fa4519
joyfulStalker <joyfulStalker@qq.com>
2020-12-17 17:55:39 +0800

    去除查询条件带_class的问题

--- helper-java/helper-webflux/src/main/java/co/imdo/perfect/config/MongoConfig.java
+++ helper-java/helper-webflux/src/main/java/co/imdo/perfect/config/MongoConfig.java
@@ -1,25 +1,19 @@
-//package co.imdo.perfect.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
-//import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-//import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
-//import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-//import org.springframework.data.mongodb.core.convert.MongoConverter;
-//
-//@Configuration
-//public class MongoConfig {
-//
-//    @Bean
-//    public DefaultMongoTypeMapper getDefaultMongoTypeMapper(){
-//        return new DefaultMongoTypeMapper(null);
-//    }
-//
-//
-//    @Bean
-//    public ReactiveMongoTemplate getReactiveMongoTemplate(ReactiveMongoDatabaseFactory databaseFactory){
-//        MongoConverter mongoConverter = new MappingMongoConverter();
-//
-//    }
-//}
+package co.imdo.perfect.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean("reactiveMongoTemplate")
+    public ReactiveMongoTemplate getReactiveMongoTemplate(ReactiveMongoDatabaseFactory databaseFactory, MappingMongoConverter mongoConverter) {
+        //去除查询条件带_class的问题
+        mongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return new ReactiveMongoTemplate(databaseFactory, mongoConverter);
+    }
+}
