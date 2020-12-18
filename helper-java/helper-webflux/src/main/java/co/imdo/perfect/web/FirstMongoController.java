5c515231cb9990940f1df37e427677676fb8891d
joyfulStalker <joyfulStalker@qq.com>
2020-12-18 11:40:34 +0800

    swagger 请求参数问题

--- helper-java/helper-webflux/src/main/java/co/imdo/perfect/web/FirstMongoController.java
+++ helper-java/helper-webflux/src/main/java/co/imdo/perfect/web/FirstMongoController.java
@@ -4,13 +4,11 @@ import co.imdo.perfect.entity.FirstMongo;
 import co.imdo.perfect.service.FirstMongoService;
 import co.imdo.perfect.vo.FirstMongoVo;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -38,17 +36,17 @@ public class FirstMongoController {
             httpMethod = "POST",
             response = FirstMongo.class,
             notes = "2、firstTestMongoInsertOne",
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
-            consumes = MediaType.TEXT_EVENT_STREAM_VALUE)
+            produces = MediaType.TEXT_EVENT_STREAM_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
     public Mono firstTestMongoInsertOne(@RequestBody FirstMongoVo insert) {
         return mongoTestService.firstTestMongoInsertOne(insert);
     }
 
     @PostMapping("/firstTestMongoById")
-    @ApiOperation(value = "1、firstTestMongoById",
+    @ApiOperation(value = "3、firstTestMongoById",
             httpMethod = "POST",
             response = FirstMongo.class,
-            notes = "1、firstTestMongoById",
+            notes = "3、firstTestMongoById",
             produces = MediaType.TEXT_EVENT_STREAM_VALUE,
             consumes = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Mono<FirstMongo> firstTestMongoById(@RequestParam("id") String id) {
