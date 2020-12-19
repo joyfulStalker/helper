85297f21f5cd7b5e7324a2e7d312211bb72e22e8
joyfulStalker <joyfulStalker@qq.com>
2020-12-19 10:46:05 +0800

    springboot+jedis+单机配置

--- helper-java/redis-sentinel/src/main/java/co/imdo/perfect/config/RedisConfig.java
+++ helper-java/redis-sentinel/src/main/java/co/imdo/perfect/config/RedisConfig.java
@@ -1,75 +1,35 @@
 package co.imdo.perfect.config;
 
-import org.apache.commons.lang.StringUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.*;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolAbstract;
 import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.JedisSentinelPool;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 /**
- * @author liusy
+ * @author liu
  * Redis 配置类
  */
-@EnableCaching
+@Slf4j
 @Configuration
+@EnableConfigurationProperties(RedisProperties.class)
 public class RedisConfig {
-
-    @Value("${spring.sentinel.enable:false}")
-    private boolean sentinelEnable;
-
-    @Value("${spring.redis.host:localhost}")
-    private String host;
-
-    @Value("${spring.redis.port:6379}")
-    private int port;
-
-    @Value("${spring.redis.password:}")
-    private String password;
-
-    @Value("${spring.redis.timeout:0}")
-    private int timeout;
-
-    @Value("${spring.redis.jedis.pool.max-idle:8}")
-    private int maxIdle;
-
-    @Value("${spring.redis.jedis.pool.max-wait:10000}")
-    private long maxWaitMillis;
-
 
     @Autowired
     private RedisProperties redisProperties;
 
     @Bean
-    public JedisPoolAbstract redisPoolFactory() {
-        if (!sentinelEnable) {
-            JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-            jedisPoolConfig.setMaxIdle(maxIdle);
-            jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
-            if (StringUtils.isNotBlank(password)) {
-                return new JedisPool(jedisPoolConfig, host, port, timeout, password);
-            } else {
-                return new JedisPool(jedisPoolConfig, host, port, timeout);
-            }
-        } else {
-
+    public JedisPoolAbstract jedisPool() {
+        if (redisProperties.getSentinel() != null) {
             List<String> nodes = redisProperties.getSentinel().getNodes();
             Set<String> nodeSets = new HashSet<>();
             for (String node : nodes) {
@@ -82,74 +42,29 @@ public class RedisConfig {
                     , redisProperties.getSentinel().getPassword()
             );
             return jedisSentinelPool;
+        } else if (redisProperties.getCluster() != null) {
+
+            return null;
+        } else {
+            return new JedisPool(getJedisPoolConfig(), redisProperties.getHost(), redisProperties.getPort(),
+                    (int) redisProperties.getTimeout().toMillis(), redisProperties.getPassword());
         }
-
     }
 
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
-        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        return redisTemplate;
-    }
 
     /**
-     * 缓存管理器
+     * jedisPoolConfig
      *
-     * @param redisConnectionFactory redis 连接工厂
-     * @return CacheManager
+     * @return
      */
-    @Bean
-    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-        RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager.RedisCacheManagerBuilder
-                .fromConnectionFactory(redisConnectionFactory);
-        return builder.build();
-    }
-
-    @Bean
-    public KeyGenerator wiselyKeyGenerator() {
-        return (target, method, params) -> {
-            StringBuilder sb = new StringBuilder();
-            sb.append(target.getClass().getName());
-            sb.append(method.getName());
-            Arrays.stream(params).map(Object::toString).forEach(sb::append);
-            return sb.toString();
-        };
-    }
-
-    @Bean
-    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        StringRedisTemplate template = new StringRedisTemplate();
-        template.setConnectionFactory(redisConnectionFactory);
-        return template;
-    }
-
-    @Bean
-    public HashOperations<String, String, Object> hashOperations(RedisTemplate<String, Object> redisTemplate) {
-        return redisTemplate.opsForHash();
-    }
-
-    @Bean
-    public ValueOperations<String, String> valueOperations(RedisTemplate<String, String> redisTemplate) {
-        return redisTemplate.opsForValue();
-    }
-
-    @Bean
-    public ListOperations<String, Object> listOperations(RedisTemplate<String, Object> redisTemplate) {
-        return redisTemplate.opsForList();
-    }
-
-    @Bean
-    public SetOperations<String, Object> setOperations(RedisTemplate<String, Object> redisTemplate) {
-        return redisTemplate.opsForSet();
-    }
-
-    @Bean
-    public ZSetOperations<String, Object> zSetOperations(RedisTemplate<String, Object> redisTemplate) {
-        return redisTemplate.opsForZSet();
+    private JedisPoolConfig getJedisPoolConfig() {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        RedisProperties.Pool pool = redisProperties.getJedis().getPool();
+        if (pool != null) {
+            jedisPoolConfig.setMaxIdle(pool.getMaxIdle());
+            jedisPoolConfig.setMaxWaitMillis(pool.getMaxWait().toMillis());
+            //其他配置根据需要配置
+        }
+        return jedisPoolConfig;
     }
 }
--- helper-java/redis-sentinel/src/main/java/co/imdo/perfect/controller/TestSentinelController.java
+++ helper-java/redis-sentinel/src/main/java/co/imdo/perfect/controller/TestSentinelController.java
@@ -1,34 +1,23 @@
 package co.imdo.perfect.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.*;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPoolAbstract;
 
 @RestController
-@RequestMapping("/sentinel")
+@RequestMapping("/redis")
 public class TestSentinelController {
 
     @Autowired
     private JedisPoolAbstract jedisPool;
 
-    @Autowired
-    private ValueOperations opsForValue;
-
     @ResponseBody
-    @GetMapping("/test1")
+    @GetMapping("/get")
     public String test1(@RequestParam("name") String name) {
         Jedis jedis = jedisPool.getResource();
         String s = jedis.get(name);
         jedis.close();
         return s;
-    }
-
-    @ResponseBody
-    @GetMapping("/temp")
-    public String redisTemplate(@RequestParam("name") String name) {
-        Object o = opsForValue.get(name);
-        return o.toString();
     }
 }
